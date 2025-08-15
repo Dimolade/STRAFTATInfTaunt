c5c3b66f49ad64d0b9a8b45c03ecfc74868f2827
Steam Deck User <dimoladeyt@gmail.com>
2025-08-15 21:32:05 +0200

    Clean

--- obj/Debug/net471/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace STRAFMOD
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "InfTaunt";
-        public const string PLUGIN_NAME = "debugPlgn";
-        public const string PLUGIN_VERSION = "6.9.4.20";
-    }
-}
